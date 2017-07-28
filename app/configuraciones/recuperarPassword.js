7e1263c6c2eeef4c406b823d5cdde4667b4abd76
Andrés Isidro Ahedo Rocha <andres.ahedo@gmail.com>
2017-07-28 14:26:45 -0500

    Comportamiento
    se suben avances al comportamiento

--- app/configuraciones/recuperarPassword.js
+++ app/configuraciones/recuperarPassword.js
@@ -5,4 +5,46 @@ angular.module('recuperar', [])
     function RecuperarControlador($http,$location){
         console.log("IniciaRecuperacion");
         var rc = this;
+        rc.password = "";
+        rc.confirmacion = "";
+        rc.usuario = {};
+        rc.usuario = $location.search();
+        rc.restablecer = restablecer;
+        
+        function restablecer(){
+            if(rc.password !== rc.confirmacion){
+                return mensaje("error","Aviso","La contraseñas no coinciden.");
+            };
+            if(rc.usuario.idUsuario === undefined || rc.usuario.key === undefined){
+                return mensaje("error","Aviso","Vigencia terminada.");
+            };
+            /*
+            $http(
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': "Bearer "+rc.usuario.key
+                    },
+                    url : "http://localhost:8080/lfs/usuarios/recuperarPassword",
+                    method : "PUT",
+                    data : {
+                        "idUsuario":rc.usuario.idUsuario,
+                        "password":rc.password
+                    }
+                }
+            ).then(
+                function(){
+                    mensajes("success","Aviso","Constraseña modificada.");
+                },
+                function(response){
+                    if(response.status === 401){
+                        mensajes("error","Aviso","Vigencia terminada.");
+                    }else{
+                        mensajes("error","Aviso","Ocurrio un error inesperado, por favor intentelo más tarde.");
+                    }
+                }
+            );
+            */
+            
+        };
     };
